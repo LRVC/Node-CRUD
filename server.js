283410ebcea46e6ac499126e54e005071d8d76df
Reece <lrv378@gmail.com>
2015-05-18 19:39:46 -0600

    Edit page functions

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var fs = require("fs"),
     http = require("http"),
     mongoose = require("mongoose"),
     jade = require('jade');
+    url = require('url');
 
-// load Car model
+
 var carAttrs = require("./car.js"),
     carSchema = mongoose.Schema(carAttrs);
 
@@ -11,33 +12,117 @@ var carAttrs = require("./car.js"),
 var Car = mongoose.model('Car', carSchema);
 mongoose.connect('mongodb://localhost/crud_sans_frameworks');
 
+
 var handleRequest = function(req, res) {
-  // redirect users to /cars if they try to hit the homepage
+  var path, objId, carId;
+
+  if (req.url == '/favicon.ico') {
+    res.end();
+    return;
+  }
+
+  console.log(req.url);
+  function findId(urlPath) {
+    var urlArr = urlPath.split('/');
+    objId = urlArr[urlArr.length - 2];
+    return objId
+  }
+
+  function parseUrlPath(urlPath) {
+    var urlArr = urlPath.split('/');
+    path = urlArr[urlArr.length - 1];
+    return path;  
+  }
+  var firstChar = parseUrlPath(req.url).charAt(0);
+  
   if (req.url == '/') {
     res.writeHead(301, {Location: 'http://localhost:1337/cars'})
     res.end();
   }
 
-  if (req.url == '/cars') {
-    // Synchronously load the index jade template (http://jade-lang.com/)
+  if (req.url == '/cars' && req.method == 'GET') {
     var index = fs.readFileSync('index.jade', 'utf8');
-    // Compile template
-    compiledIndex = jade.compile(index, { pretty: true, filename: 'index.jade' });
+    var compiledIndex = jade.compile(index, { pretty: true, filename: 'index.jade' });
 
-    // example of data that can be passed in to the Jade template:
-    // in your CRUD app, a call to Mongoose should return all of the Cars
-    var sampleDataForCars = { cars: [
-      { driver: 'Andreas', make: 'Nissan', model: 'Xterra', year: 2005 },
-      { driver: 'Bob Ross', make: 'Ford', model: 'Pinto', year: 1972 }
-    ]};
+    Car.find({}, function(err, cars) {
+      var rendered = compiledIndex({cars: cars});
+      res.end(rendered);
+    })
 
-    // Render jade template, passing in the info
-    var rendered = compiledIndex(sampleDataForCars);
+  } else if( req.url == '/cars/new') {
+    
+    var newCar = fs.readFileSync('new.jade', 'utf8');
+    var compiledNew = jade.compile(newCar, { pretty: true, filename: 'new.jade' });
 
-    // Write rendered contents to response stream
-    res.end(rendered);
+
+    res.end(compiledNew());
+  } else if(req.url == '/cars' && req.method == 'POST') {
+    var postParams = {};
+    req.on('data', function(data) {
+      var dataArray = data.toString().split("&");
+      for (var i = 0; i < dataArray.length; i++) {
+        var newArray = dataArray[i].split("=");
+        postParams[newArray[0]] = newArray[1];
+      }
+      console.log(postParams);
+      
+    var car = new Car(postParams);
+    car.save(function (err) {
+      if (err) {
+        console.log(err)
+      }
+    });
+
+    res.writeHead(301, {Location: 'http://localhost:1337/cars'});
+    res.end();
+    });
+  } else if(parseUrlPath(req.url) == 'show') {
+    
+    var show = fs.readFileSync('show.jade', 'utf8');
+    var compiledShow = jade.compile(show, { pretty: true, filename: 'show.jade' });
+    carId = findId(req.url);
+    Car.find({_id: carId}, function(err, car) {
+      var rendered = compiledShow({cars: car});
+      console.log(rendered);
+      res.end(rendered);    
+    });
+  } else if(parseUrlPath(req.url) == 'edit') {
+
+    var editCar = fs.readFileSync('edit.jade', 'utf8');
+    var compiledEdit = jade.compile(editCar, { pretty: true, filename: 'edit.jade' });
+    carId = findId(req.url);
+    console.log(carId);
+
+    Car.findOne({'_id': carId}, function(err, car) {
+      console.log(car);
+      var rendered = compiledEdit({car: car});
+      res.end(rendered);    
+    });
+
+
+    
+  } else if(parseUrlPath(req.url) == 'update' && req.method == 'POST') { 
+    var postParams = {};
+    req.on('data', function(data) {
+      var dataArray = data.toString().split("&");
+      for (var i = 0; i < dataArray.length; i++) {
+        var newArray = dataArray[i].split("=");
+        postParams[newArray[0]] = newArray[1];
+      }
+    });
+
+
+
+  } else if(!isNaN(firstChar)) {
+    carId = parseUrlPath(req.url);
+
+    Car.remove({_id: carId}, function(err, car) {
+      if (err) throw err;
+
+    })
+      
+
   } else {
-    // Your code might go here (or it might not)
     res.writeHead(200);
     res.end('A new programming journey awaits');
   }
